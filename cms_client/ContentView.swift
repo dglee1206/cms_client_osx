cfb72c282d65ccb25968316eb1fde3fef687a579
DoGyeong Lee <dglee1206@gmail.com>
2020-02-09 10:08:51 +0900

    first init

---
+++ cms_client/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  cms_client
+//
+//  Created by DoGyeong Lee on 2020/02/09.
+//  Copyright © 2020 DoGyeong Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
